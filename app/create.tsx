6b583d758b5cada70570d57f5399919a90f51e9a
Shuvo Halder <shuvohalder499@gmail.com>
2025-12-20 21:42:21 +0600

    create a home page

---
+++ app/create.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function CreateTodo() {
+  return (
+    <View>
+      <Text>CreateTodo</Text>
+    </View>
+  )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
+import { Link } from 'expo-router';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 interface Todo {
   id: number;
@@ -11,21 +12,86 @@ const initialTodo: Todo[] = [
   {id: 1, text: "leaen React Native", done: true},
   {id: 2, text: "Learn React", done: false},
 ];  
-
+initialTodo
 
 const Index = () => {
-  const [todos, setTodos] = useState<Todo[]>(initialTodo)
+  const [todos, setTodos] = useState<Todo[]>(initialTodo);
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.todoContainer}>
+        <Text style={styles.todoHeader}>Todos</Text>
+
+
+        {todos.map((todo) => (
+          <View key={todo.id} style={styles.todoCard}>
+            <Text style={styles.todoTitle}>{todo.text}</Text>
+          </View>
+        ))}
+      </View>
       <View>
-        <Text>Todos</Text>
+        <Link asChild href="/create">
+          <TouchableOpacity style={styles.createTodoButton}
+          activeOpacity={0.6}>
+            <Text style={styles.createTodoButtonText}>Create Todo</Text>
+          </TouchableOpacity>
+        </Link>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+  },
+
+  todoContainer: {
+    marginVertical: 5,
+    padding: 10,
+  },
+
+  todoHeader: {
+    fontSize: 24,
+    fontWeight: "bold", 
+  },
+
+  // todo card styles
+  todoCard: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    marginVertical: 10,
+    backgroundColor: "#f8f8f8",
+    borderRadius: 5,
+    elevation: 2,
+  },
+  
+  todoTitle: {
+    fontSize: 18,
+    fontWeight: "500",
+  },
+
+  // create todo button styles
+  createTodoButton: {
+    backgroundColor: "#000",
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+    margin: 10,
+    borderRadius: 30,
+    alignItems: "center",
+    marginTop: 10,
+  },
+
+  createTodoButtonText: {
+    color: "#fff",
+    fontSize: 18,
+  },
 })
+
 
 export default Index;
 
